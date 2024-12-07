ae293906a3b0a3d467028dc70048b91d708b6ce1
Samurai_Chia <jyjzone@163.com>
2024-12-07 16:55:49 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("******the value of s1 : {s1}******** the value of s2 : {s2}*******");
+
+    /*
+    let  s1_address = s1.as_ptr();
+    let  s2_address = s2.as_ptr();
+    println!("******the value of s1_address : {s1_address}");
+    println!("******the value of s2_address : {s2_address}");
+     */
 }
